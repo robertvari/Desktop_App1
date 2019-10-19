061cb14d4a431c8fe3ab3958d6d0697b0123a835
Robert Vari <mail.robertvari@gmail.com>
2019-10-19 17:28:27 +0200

    Custom Signals example: Final

--- custom_signals.py
+++ custom_signals.py
@@ -1,7 +1,10 @@
 from PySide2.QtWidgets import QApplication, QWidget, QVBoxLayout, QDial, QLabel
+from PySide2.QtCore import Signal
 import sys
 
 class CustomSignal(QWidget):
+    pressure = Signal()
+
     def __init__(self):
         super(CustomSignal, self).__init__()
 
@@ -11,9 +14,19 @@ class CustomSignal(QWidget):
         main_layout.addWidget(self.dial)
 
         self.allert_label = QLabel()
+        main_layout.addWidget(self.allert_label)
+
+        self.dial.sliderMoved.connect(self.check_value)
+
+        self.pressure.connect(self.pressure_allert)
+
+    def check_value(self, value):
+        if value >= 50:
+            self.pressure.emit()
 
     def pressure_allert(self):
         self.allert_label.setText("To much pressure!!!")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
