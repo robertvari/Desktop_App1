7a0b9879a4f8d435644146da7378579e1030408b
Robert Vari <mail.robertvari@gmail.com>
2019-10-19 16:39:40 +0200

    Contact form layout

--- contact_form.py
+++ contact_form.py
@@ -4,6 +4,31 @@ import sys, json
 class ContactForm(QWidget):
     def __init__(self):
         super(ContactForm, self).__init__()
+        self.setWindowTitle("My Contact Form")
+        self.resize(400, 0)
+
+        # main layout
+        main_layout = QVBoxLayout(self)
+
+        # line edits
+        name_field = QLineEdit()
+        name_field.setPlaceholderText("Name...")
+        main_layout.addWidget(name_field)
+
+        phone_field = QLineEdit()
+        phone_field.setPlaceholderText("Phone...")
+        main_layout.addWidget(phone_field)
+
+        email_field = QLineEdit()
+        email_field.setPlaceholderText("Email...")
+        main_layout.addWidget(email_field)
+
+        address_field = QLineEdit()
+        address_field.setPlaceholderText("Address...")
+        main_layout.addWidget(address_field)
+
+        save_btn = QPushButton("Save")
+        main_layout.addWidget(save_btn)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
